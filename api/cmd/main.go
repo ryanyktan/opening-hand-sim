e40a3b1a86bcaf0103b6230ab39d452e086b9dce
ryantanyekai <CRTY422D@spgroup.com.sg>
2023-11-08 17:22:11 +0800

    added api folder and vendored tcg api sdk

---
+++ api/cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	tcg "github.com/PokemonTCG/pokemon-tcg-sdk-go-v2/pkg"
+	"github.com/PokemonTCG/pokemon-tcg-sdk-go-v2/pkg/request"
+)
+
+func main() {
+	// If an empty string is used here, you can stil use the API with stricter limits.
+	// See: https://docs.pokemontcg.io/#documentationrate_limits
+	c := tcg.NewClient("your_api_key")
+
+	cards, err := c.GetCards(
+		request.Query("name:jirachi", "types:psychic"),
+		request.PageSize(5),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, card := range cards {
+		log.Println(card.Name)
+	}
+}
---
+++ api/go.mod
@@ -0,0 +1,5 @@
+module github.com/ryanyktan/opening-hand-sim/api
+
+go 1.21.1
+
+require github.com/PokemonTCG/pokemon-tcg-sdk-go-v2 v0.1.1 // indirect
