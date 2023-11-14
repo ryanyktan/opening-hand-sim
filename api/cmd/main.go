dd7e2920c5a37d9ed8209d2feece91f6a682c340
ryantanyekai <CRTY422D@spgroup.com.sg>
2023-11-14 15:57:16 +0800

    Add parsing data for basic energy (all parse to sve, fairy to sm energy

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+
+	tcg "github.com/PokemonTCG/pokemon-tcg-sdk-go-v2/pkg"
+	"github.com/PokemonTCG/pokemon-tcg-sdk-go-v2/pkg/request"
+)
+
+const apiKey string = "f028221e-b952-4b18-9f83-22d507a4ed7b"
+
 func main() {
 	// do something
+	c := tcg.NewClient(apiKey)
+
+	cards, err := c.GetCards(
+		request.Query("name:Lightning Energy"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, card := range cards {
+		fmt.Println(card.ID)
+	}
 }
