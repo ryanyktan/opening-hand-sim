59ff7153995f7e4047def53d5638cf56c70ba445
ryantanyekai <CRTY422D@spgroup.com.sg>
2023-11-22 17:32:15 +0800

    Complete hand generation logic

--- api/controller/generatehands.go
+++ api/controller/generatehands.go
@@ -14,12 +14,13 @@ func (i impl) GenerateOpeningHands(deck []tcg.PokemonCard, n int) []OpeningHand 
 		hand := make([]tcg.PokemonCard, 7)
 		prizes := make([]tcg.PokemonCard, 6)
 		mulligans := 0
+		handNotValid := true
 
 		s := rand.NewSource(time.Now().UnixNano())
 		r := rand.New(s)
 
 		// some logic
-		for {
+		for handNotValid {
 			for j := 0; j < 7; j++ {
 				randomInt := r.Intn(60 - j)
 				if randomInt == 59-j {
@@ -29,11 +30,22 @@ func (i impl) GenerateOpeningHands(deck []tcg.PokemonCard, n int) []OpeningHand 
 			}
 
 			// check for valid opening hand
-			// hand := deck[53:]
+			hand = deck[53:]
 			// if hand is valid, generate prizes and break
-			break
-			// otherwise, increase mulligan
+			if isValidHand(hand) {
+				for j := 7; j < 13; j++ {
+					randomInt := r.Intn(60 - j)
+					if randomInt == 59-j {
+						continue
+					}
+					deck[randomInt], deck[59-j] = deck[59-j], deck[randomInt]
+				}
+				prizes = deck[47:53]
+				handNotValid = false
+			}
 
+			// otherwise, increase mulligan
+			mulligans++
 		}
 
 		res = append(res, OpeningHand{
@@ -49,4 +61,20 @@ type OpeningHand struct {
 	Hand      []tcg.PokemonCard
 	Prizes    []tcg.PokemonCard
 	Mulligans int
+}
+
+// A valid hand contains 7 cards and at least 1 basic pokemon.
+func isValidHand(hand []tcg.PokemonCard) bool {
+	if len(hand) != 7 {
+		return false
+	}
+
+	for _, card := range hand {
+		for _, subtype := range card.Subtypes {
+			if subtype == "Basic" {
+				return true
+			}
+		}
+	}
+	return false
 }
