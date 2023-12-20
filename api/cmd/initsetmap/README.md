e3f251b9379d7f62166019d9e35eb418978a4f45
ryanyktan <crty422d@spgroup.com.sg>
2023-12-20 16:03:45 +0800

    Add blank component

--- api/cmd/initsetmap/README.md
+++ api/cmd/initsetmap/README.md
@@ -1,1 +1,1 @@
-do not run unless setmap.csv is missing from controller folder
+do not run unless setmap.csv is missing from service/setmap folder
---
+++ web/src/components/OpeningHandSim.jsx
@@ -0,0 +1,14 @@
+const OpeningHandSim = () => {
+
+    return (
+        // TODO: Form for decklist input, number of simulations
+        <div>
+            <p>
+                opening hand simulator
+            </p>
+            <img src="https://images.pokemontcg.io/sv1/245.png" alt="pic" />
+        </div>
+    );
+}
+
+export default OpeningHandSim
