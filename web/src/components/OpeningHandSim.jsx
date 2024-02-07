463f124c3e11c91eaccb521d6d15443c8a35a17a
ryanyktan <crty422d@spgroup.com.sg>
2024-02-07 14:07:19 +0800

    add form to frontend

--- web/src/components/OpeningHandSim.jsx
+++ web/src/components/OpeningHandSim.jsx
@@ -1,12 +1,41 @@
+import React from 'react';
+import { Form, Field, Formik, ErrorMessage } from "formik";
+
 const OpeningHandSim = () => {
 
     return (
-        // TODO: Form for decklist input, number of simulations
         <div>
             <p>
                 opening hand simulator
             </p>
-            <img src="https://images.pokemontcg.io/sv1/245.png" alt="pic" />
+            <Formik
+                initialValues={{
+                    decklist: '',
+                    numberOfSimulations: '',
+                }}
+                onSubmit={async (values) => {
+                    await new Promise((r) => setTimeout(r, 500));
+                    alert(JSON.stringify(values, null, 2));
+                }}
+            >
+                <Form>
+                    <label htmlFor="decklist">Decklist</label>
+                    <Field id="decklist" name="decklist" placeholder="Enter your decklist here" />
+
+                    <div>
+                        <label htmlFor="numberOfSimulations">Number Of Simulations:</label>
+                        <Field name="numberOfSimulations" as="select" label="Number of Simulations">
+                            <option value="">Choose how many simulations to run</option>
+                            <option value="10">10</option>
+                            <option value="100">100</option>
+                            <option value="1000">1000</option>
+                        </Field>
+                        <ErrorMessage name="numberOfSimulations" component="div" />
+                    </div>
+
+                    <button type="submit">Submit</button>
+                </Form>
+            </Formik>
         </div>
     );
 }
